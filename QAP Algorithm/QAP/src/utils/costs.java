a29e860cba227de155f972f8abe6c746859bbf81
Mihai Ionut Aftinescu <mihaiaftinescu@gmail.com>
2022-10-02 12:17:46 +0200

    Made Costs static for use

--- QAP Algorithm/QAP/src/utils/costs.java
+++ QAP Algorithm/QAP/src/utils/costs.java
@@ -1,21 +1,14 @@
 package utils;
 
-public class costs {
+public class Costs {
 
-    int[][] value;
-    int[][] distance;
 
-    public costs(int[][] values,int [][] dist){
-        this.value = values;
-        this.distance = dist;
-    }
-
-    public int getCost(int[] solArray) {
+    public static int getCost(int [][] values,int [][] distance, int[] solArray) {
          int sol = 0;
 
          for(int i = 0; i < solArray.length; i++){
              for(int j = 0; j < solArray.length; j++){
-                 sol += value[i][j] * distance[solArray[i]][solArray[j]];
+                 sol += values[i][j] * distance[solArray[i]][solArray[j]];
              }
          }
          return sol;
